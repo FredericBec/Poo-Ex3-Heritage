76d41d9d64eca43083ce535c777f45ed7d60dd9c
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-15 10:03:37 +0100

    modification setResidentNumber, gestion de réduction de la population

--- src/fr/fms/entities/City.java
+++ src/fr/fms/entities/City.java
@@ -35,6 +35,8 @@ public class City {
 	public void setResidentNumber(int residentNumber) {
 		if(residentNumber < 0) {
 			System.out.println("Le nombre d'habitant ne peut être négatif");
+		}else if(residentNumber < this.residentNumber){
+			System.out.println("Impossible de réduire la population !!");
 		}else {			
 			this.residentNumber = residentNumber;
 		}
